7ebd6aa4370eabd8947fa5f8dbbfa241a98dff43
Till Neunast <till.neunast@chorus.co.nz>
2017-06-12 18:06:44 +1200

    Workaround to replace help link to JMeter-Plugins.org

--- src/main/java/nz/co/breakpoint/jmeter/vizualizers/sshmon/SSHMonGui.java
+++ src/main/java/nz/co/breakpoint/jmeter/vizualizers/sshmon/SSHMonGui.java
@@ -21,10 +21,50 @@ public class SSHMonGui
         "", "", 22, "", "", "", "", false
     };
     protected static int[] columnWidths = new int[]{
-        100, 100, 50, 100, 100, 100, 500, 20
+        100, 100, 50, 100, 100, 100, 500, 50
     };
 
     public static final String WIKI_PAGE = "https://github.com/tilln/jmeter-sshmon";
+
+    // WORKAROUND to replace the help link added by JMeterPluginsUtils.addHelpLinkToPanel
+    public SSHMonGui() {
+        org.apache.jmeter.gui.GuiPackage guiPack = org.apache.jmeter.gui.GuiPackage.getInstance();
+        if (guiPack != null) {
+            org.apache.jmeter.gui.JMeterGUIComponent gui = guiPack.getCurrentGui();
+            if (gui instanceof SSHMonGui) {
+                replaceHelpLinkPanel((SSHMonGui)gui);
+            }
+        }
+    }
+
+    private static void replaceHelpLinkPanel(javax.swing.JComponent outer) {
+        for (int n = 0; n < outer.getComponentCount(); n++) {
+            if (outer.getComponent(n) instanceof javax.swing.JComponent) {
+                javax.swing.JComponent comp = (javax.swing.JComponent) outer.getComponent(n);
+                if (comp instanceof javax.swing.JPanel && ((java.awt.Container)comp).getComponentCount() == 3) {
+                    java.awt.Component link = ((java.awt.Container)comp).getComponent(1);
+                    if (link instanceof javax.swing.JLabel && ((javax.swing.JLabel)link).getText().equals("Help on this plugin")) {
+                        link.removeMouseListener(link.getMouseListeners()[0]);
+                        link.addMouseListener(new java.awt.event.MouseAdapter() {
+                            @Override
+                            public void mouseClicked(java.awt.event.MouseEvent e) { kg.apc.jmeter.JMeterPluginsUtils.openInBrowser(WIKI_PAGE); }
+                            @Override
+                            public void mousePressed(java.awt.event.MouseEvent e) {}
+                            @Override
+                            public void mouseReleased(java.awt.event.MouseEvent e) {}
+                            @Override
+                            public void mouseEntered(java.awt.event.MouseEvent e) {}
+                            @Override
+                            public void mouseExited(java.awt.event.MouseEvent e) {}
+                        });
+                        return;
+                    }
+                }
+                replaceHelpLinkPanel(comp);
+            }
+        }
+    }
+    // /WORKAROUND END
 
     @Override
     protected String[] getColumnIdentifiers() { return columnIdentifiers; }
